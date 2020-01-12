dfd451b9e6ba4e9c2f679afff1d3bd30c4070083
Leina33 <dominickrutto@gmail.com>
2020-01-13 00:13:23 +0300

    remove regex exp to let the server load the page lively

--- awardsproject/urls.py
+++ awardsproject/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-     url(r'^awardsapp/',include('awardsapp.urls')),
+     url(r'',include('awardsapp.urls')),
 ]
