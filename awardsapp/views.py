2eb375f53c64215cb875e56286d3a23b96b017ef
Leina33 <dominickrutto@gmail.com>
2020-01-15 13:36:03 +0300

    update the views file

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -13,7 +13,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 # from .serializers import ProjectSerializer, ProfileSerializer
 from rest_framework import status
-# from .permissions import IsAdminOrReadOnly
+from .permissions import IsAdminOrReadOnly
 
 
 
